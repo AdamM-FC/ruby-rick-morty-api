1dbe28951dc164f403581d276cae3f62de19efcc
Adam Makhfoudi <adam.makhfoudi@fundingcircle.com>
2020-08-18 09:40:07 +0100

    Adds CRUD for characters
    Adds unit tests

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,19 +1,37 @@
 class CharactersController < ApplicationController
+   before_action :set_character, only: %i[show update destroy]
+
   def index
     @characters = Character.all
     json_response(@characters)
   end
 
   def create
+    @character = Character.create!(character_params)
+    json_response(@character, :created)
   end
 
   def show
+    json_response(@character)
   end
 
   def update
+    @character.update(character_params)
+    head :no_content
   end
 
   def destroy
+    @character.destroy
+    head :no_content
   end
 
+  private
+
+  def character_params
+    params.permit(:name, :species, :status, :character_type, :gender, :image)
+  end
+
+  def set_character
+    @character = Character.find(params[:id])
+  end
 end
--- spec/requests/characters_spec.rb
+++ spec/requests/characters_spec.rb
@@ -1,15 +1,101 @@
 require 'rails_helper'
 
 RSpec.describe 'Characters API', type: :request do
-let!(:characters) { create_list(:character, 10) }
-let(:character_id) { characters.first.id }
+  let!(:characters) { create_list(:character, 10) }
+  let(:character_id) { characters.first.id }
 
-    describe 'Get /characters' do 
-        before { get '/characters'}
+  # GET
+  describe 'Get /characters' do
+    before { get '/characters' }
 
-        it 'returns characters' do 
-            expect(json).not_to be_empty
-            expect(json.size).to eq(10)
-        end
+    it 'returns characters' do
+      expect(json).not_to be_empty
+      expect(json.size).to eq(10)
     end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  # POST
+  describe 'Post /characters' do
+    context 'when the request is valid' do
+      name = 'Rick'
+      species = 'Alive'
+      status = 'Alive'
+      character_type = 'Character'
+      gender = 'Male'
+      image = 'N/A'
+
+      let(:valid_attributes) do
+        { name: name,
+          species: species,
+          status: status,
+          character_type: character_type,
+          gender: gender,
+          image: image }
+      end
+
+      before { post '/characters', params: valid_attributes }
+
+      it 'creates a character' do
+        expect(json['name']).to eq(name)
+        expect(json['species']).to eq(species)
+        expect(json['status']).to eq(status)
+        expect(json['character_type']).to eq(character_type)
+        expect(json['gender']).to eq(gender)
+        expect(json['image']).to eq(image)
+      end
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when the request is invalid' do
+      before do
+        post '/characters', params: { species: 'Alive',
+                                      status: 'Human',
+                                      character_type: 'Character',
+                                      gender: 'Male',
+                                      image: 'N/A' }
+      end
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a validation failure message' do
+        expect(response.body)
+          .to match(/Validation failed: Name can't be blank/)
+      end
+    end
+  end
+
+  # PUT
+  describe 'Put /characters/:id' do
+    let(:valid_attributes) { { name: 'test' } }
+
+    context 'when the record exists' do
+      before { put "/characters/#{character_id}", params: valid_attributes }
+
+      it 'updates the record' do
+        expect(response.body).to be_empty
+      end
+
+      it 'returns status code 204' do
+        expect(response).to have_http_status(204)
+      end
+    end
+  end
+
+  # DELETE
+  describe 'DELETE /characters/:id' do
+    before { delete "/characters/#{character_id}" }
+
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204)
+    end
+  end
 end
