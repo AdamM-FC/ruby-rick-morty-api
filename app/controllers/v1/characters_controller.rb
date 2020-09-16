4e5ca545f7b61215196707ecd355ffb7673f67aa
Stefan Lage <stefan.lage@fundingcircle.com>
2020-09-16 18:38:55 +0100

    WIP

--- app/controllers/v1/characters_controller.rb
+++ app/controllers/v1/characters_controller.rb
@@ -1,27 +1,39 @@
 module V1
+  require_relative './raw_data_topic_producer'
+
   class CharactersController < ApplicationController
     before_action :set_character, only: %i[show update destroy]
+
+    def initialize
+      @dataTopicProducer = RawDataTopicProducer.new
+    end
 
     def index
       serialize_object_with_links(Character, CharacterSerializer)
     end
 
-    def create
-      @character = Character.create!(character_params)
-      json_response(@character, :created)
-    end
-
+    # GET
     def show
       json_response(@character)
     end
 
-    def update
-      @character.update(character_params)
+    # POST
+    def create
+      character_params.require([:name, :species, :status, :character_type, :gender, :image])
+      @dataTopicProducer.produce(:POST, character_params)
       head :no_content
     end
 
+    # PATCH
+    def update
+      @dataTopicProducer.produce(:PATCH, character_params)
+      head :no_content
+    end
+
+    # DELETE
     def destroy
-      @character.destroy
+      params.require(:id)
+      @dataTopicProducer.produce(:DELETE, params)
       head :no_content
     end
 
