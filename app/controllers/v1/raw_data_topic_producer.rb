43abc1828f1148a5a6c1f15ae3f1d02715602143
Andrea Mugnaini <andrea.mugnaini@fundingcircle.com>
2020-09-17 07:25:21 +0100

    Adds transformer topic subscriber

--- app/controllers/v1/raw_data_topic_producer.rb
+++ app/controllers/v1/raw_data_topic_producer.rb
@@ -1,24 +1,23 @@
 module V1
-    require 'kafka'
-    require 'json'
+  require 'kafka'
+  require 'json'
 
-    class RawDataTopicProducer
-        def initialize
-            seed_brokers = ['localhost:9092']
-            @topic_name = 'raw-data-topic'
-            client = Kafka.new(seed_brokers)
-            @producer = client.producer
-        end
-
-        def produce(action, params)
-            json = [
-                :action => action,
-                :data => params.as_json
-            ].to_json
-
-            @producer.produce(json, topic: @topic_name)
-            @producer.deliver_messages
-        end
-        
+  class RawDataTopicProducer
+    def initialize
+      seed_brokers = ['localhost:9092']
+      @topic_name = 'raw-data-topic'
+      client = Kafka.new(seed_brokers)
+      @producer = client.producer
     end
+
+    def produce(action, params)
+      json = [
+        :action => action,
+        :data => params.as_json
+      ].to_json
+
+      @producer.produce(json, topic: @topic_name)
+      @producer.deliver_messages
+    end
+  end
 end
--- app/controllers/v1/transformer_topic_consumer.rb
+++ app/controllers/v1/transformer_topic_consumer.rb
@@ -1,25 +1,48 @@
 module V1
-    require 'kafka'
+  require 'kafka'
 
-    class TransformerTopicConsumer
-        def initialize
-            seed_brokers = ['localhost:9092']
-            @topic_name = 'transform-topic'
-            client = Kafka.new(seed_brokers)
-            records = client.fetch_messages(topic: topic_name, partition: 0, offset: :earliest)
-        end
-
-        def create
-            # @character = Character.create!(character_params)
-            # json_response(@character, :created)
-        end
-
-        def update
-            # @character.update(character_params)
-        end
-
-        def destroy
-            # @character.destroy
-        end
+  class TransformerTopicConsumer
+    def initialize
+      seed_brokers = ['localhost:9092']
+      @topic_name = 'transform-topic'
+      @client = Kafka.new(seed_brokers)
+      subscribe
     end
+
+    def subscribe
+      @consumer = @client.consumer(group_id: 'rick-morty-transformer')
+      @consumer.subscribe(@topic_name)
+      @consumer.each_message do |message|
+        json = transform_json(message.value)
+        next if json[:timestamp].nil?
+        
+        action = json[:action]
+        data = json[:data]
+
+        case action
+        when :CREATE
+          create(data)
+        when :UPDATE
+          update(data)
+        when :DESTROY
+          destroy(data)
+        else
+          p 'Message not supported'
+        end
+      end
+    end
+
+    def create(data)
+      # @character = Character.create!(character_params)
+      # json_response(@character, :created)
+    end
+
+    def update(data)
+      # @character.update(character_params)
+    end
+
+    def destroy(data)
+      # @character.destroy
+    end
+  end
 end
