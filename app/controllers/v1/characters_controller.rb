e3430226f04feca4be4f8f6ca8f2309de81e9f70
Adam Makhfoudi <adam.makhfoudi@fundingcircle.com>
2020-09-18 15:39:50 +0100

    Moves kafka classes into their own folder
    Code clean up

--- app/controllers/v1/characters_controller.rb
+++ app/controllers/v1/characters_controller.rb
@@ -22,7 +22,7 @@ module V1
       RAW_DATA_PRODUCER.produce(:PATCH, params.permit(:id, *permitted_params))
       head :no_content
     end
- 
+
     def destroy
       params.require(:id)
       RAW_DATA_PRODUCER.produce(:DELETE, params)
@@ -31,8 +31,8 @@ module V1
 
     private
 
-    def permitted_params 
-      [:name, :species, :status, :character_type, :gender, :image, :location_id, :origin_id]
+    def permitted_params
+      %i[name species status character_type gender image location_id origin_id]
     end
 
     def set_character
--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -5,7 +5,6 @@ KAFKA_SEED_BROKERS = ['localhost:9092']
 RAW_DATA_TOPIC = 'raw-data-topic'
 TRANSFORMED_DATA_TOPIC = 'transform-topic'
 
-RAW_DATA_PRODUCER = V1::RawDataTopicProducer.new
-TRANSFORMER_DATA_CONSUMER = V1::TransformerTopicConsumer.new
-
-KAFKA_TRANSFORMER = KafkaTransformer.new 
+RAW_DATA_PRODUCER = RawDataTopicProducer.new
+TRANSFORMER_DATA_CONSUMER = TransformerTopicConsumer.new
+KAFKA_TRANSFORMER = KafkaTransformer.new
